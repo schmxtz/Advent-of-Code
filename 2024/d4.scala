1634f0ef8eeea10f68d8c38ae3fe8297ab44f44d
schmxtz <56893529+schmxtz@users.noreply.github.com>
2024-12-04 22:09:18 +0800

    day 4 scale setup

---
+++ 2024/d4.scala
@@ -0,0 +1,20 @@
+/* 
+Input: Lines of string
+Output: Count of how many times the string XMAS was found (count in all directions)
+Example:
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+Output: 18
+*/
+
+val path: os.Path = os.root / "usr" / "share" / "dict" / "words"
+val content: String = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX"
+
