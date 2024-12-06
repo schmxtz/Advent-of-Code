2c6d2fef5c77f219acd60d41b9d488756e68ee63
schmxtz <56893529+schmxtz@users.noreply.github.com>
2024-12-07 00:00:41 +0800

    day 6 issues

--- 2024/d6.py
+++ 2024/d6.py
@@ -1,87 +1,84 @@
 from pprint import pprint
 
-def get_next_index(cursor, row, col):
+
+# cursor information is encoded as 4 byte number 
+def get_next_index(cursor: int, row: int, col: int) -> tuple[int, int]:
     match cursor:
-        case '^':
+        case 0b0001: # ^
             return (row-1, col)
-        case '>':
+        case 0b0010: # >
             return (row, col+1)
-        case 'v':
+        case 0b0100: # v
             return (row+1, col)
-        case '<':
+        case 0b1000: # <
             return (row, col-1)
+        
+def four_byte_rotate_left(cursor: int) -> int:
+    # 1000 -> 0001, 0001 -> 0010 ...
+    return ((cursor << 1) | ((cursor >> 3) & 1)) & 15
 
-data = []
-with(open('./data/day6.txt') as file):
-    row = 0
-    col = -1
-    line_ctr = 0
-    while (line:= file.readline()):
-        line = line.strip()
-        data.append(list(line))
-        if (idx:=line.find('^'))!= -1: row, col = line_ctr, idx
-        line_ctr += 1
-start_row = row
-start_col = col
-
-direction = 0
-directions = ['^', '>', 'v', '<']
-while(True):
-    cursor = data[row][col]
-    if not isinstance(cursor, str):
-        cursor = cursor[-1]
-    (next_row, next_col) = get_next_index(cursor, row, col)
-
-    if not (0 <= next_row < len(data) and 0 <= next_col < len(data[next_row])): break
-    next_tile = data[next_row][next_col]
-    if not isinstance(next_tile, str):
-        next_tile = next_tile[-1]
-    match next_tile:
+def map_input(char: str) -> int:
+    match char:
         case '.':
-            if isinstance(data[row][col], list):
-                data[row][col].append(cursor)
-            else:
-                data[row][col] = cursor
-            data[next_row][next_col] = cursor
-            row = next_row
-            col = next_col
+            return 0
         case '#':
-            direction = (direction + 1) % 4
-            if isinstance(data[row][col], list):
-                data[row][col].append(directions[direction])
-            else:
-                data[row][col] = [directions[direction]]
+            return -1
+        case '^':
+            return 0b0001
         case _:
-            if isinstance(data[next_row][next_col], list):
-                data[next_row][next_col].append(cursor)
-            else:
-                data[next_row][next_col] = [next_tile, cursor]
-            row = next_row
-            col = next_col
+            raise Exception(f"Unknown input: {char}")
 
-row = start_row
-col = start_col
-obstruction_ctr = 0
-direction = 0
-cursor = '^'
-while(True):
-    print(row, col)
-    (next_row, next_col) = get_next_index(cursor, row, col)
-    if not (0 <= next_row < len(data) and 0 <= next_col < len(data[next_row])): break
-    next_tile = data[next_row][next_col]
-    match next_tile:
-        case '#':
-            print(next_tile)
-            direction = (direction + 1) % 4
-            cursor = directions[direction]
-        case _:
-            new_cursor = directions[(direction + 1) % 4]
-            if new_cursor in data[row][col] or :
-                obstruction_ctr += 1
-            row = next_row
-            col = next_col
+def check_with_new_obstacle(data: int[int[]], row: int, col: int, cursor]):
+    while True:
 
+if __name__ == '__main__':
+    data = []
+    with(open('./data/day6.txt') as file):
+        line_ctr = 0
+        row = -1
+        col = -1
+        while (line:= file.readline()):
+            if row == -1 and (idx:=line.find('^'))!= -1: row, col = line_ctr, idx
+            line = list(map(map_input, line.strip()))
+            data.append(list(line))
+            line_ctr += 1
+    start_row = row
+    start_col = col
 
-print(obstruction_ctr)
+    # Traverse entire map and save visited tiles' informatio (in which direction it has been crossed)
+    next_cursor = 0b0001
+    while(True):
+        cursor = next_cursor
+        next_row, next_col = get_next_index(cursor, row, col) 
+        if not (0 <= next_row < len(data) and 0 <= next_col < len(data[next_row])): break
+
+        next_tile = data[next_row][next_col]
+        match next_tile:
+            case -1: # next tile is an obstacle (#)
+                next_cursor = four_byte_rotate_left(cursor)
+            case _: # next tile can be visited
+                data[row][col] |= cursor
+                data[next_row][next_col] |= cursor
+                row, col = next_row, next_col
+
+    row = start_row
+    col = start_col
+    obstruction_ctr = 0
+    next_cursor = 0b0001
+    while(True):
+        cursor = next_cursor
+        next_row, next_col = get_next_index(cursor, row, col) 
+        if not (0 <= next_row < len(data) and 0 <= next_col < len(data[next_row])): break
+
+        next_tile = data[next_row][next_col]
+        match next_tile:
+            case -1: # next tile is an obstacle (#)
+                next_cursor = four_byte_rotate_left(cursor)
+            case _: # next tile can be visited
+                data[row][col] |= cursor
+                data[next_row][next_col] |= cursor
+                row, col = next_row, next_col
+
+    print(obstruction_ctr)
 
 
