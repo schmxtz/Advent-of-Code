31955b3009bae714907a7b95f3ec64b55cb1136f
schmxtz <56893529+schmxtz@users.noreply.github.com>
2024-12-11 19:49:53 +0800

    day 3 part 2 finished

---
+++ 2024/day03_p02.py
@@ -0,0 +1,22 @@
+import time
+import re
+
+file = open('./data/day3.txt')
+chars = file.read()
+
+start = time.perf_counter()
+pattern = r'mul\((\d*)\,(\d*)\)|(do\(\))|(don\'t\(\))'
+matches = re.findall(pattern, chars)
+
+result = 0
+skip_instruction = False
+for match in matches:
+    if match[2]: skip_instruction = False   # matched do
+    if match[3]: skip_instruction = True    # matched don't
+
+    if not skip_instruction and match[0] and match[1]:
+        result += int(match[0]) * int(match[1])
+end = time.perf_counter()
+
+duration_ns = (end-start)
+print(f"It took {duration_ns}ns to calculate {result}")
