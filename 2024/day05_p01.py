8de0acca5a1f6f0c991f9622a2ce4be8f611899c
schmxtz <56893529+schmxtz@users.noreply.github.com>
2024-12-12 16:37:13 +0800

    day 5 part 1 finished

---
+++ 2024/day05_p01.py
@@ -0,0 +1,26 @@
+import time
+
+def is_in_order(page_order, rules):
+    for i in range(len(page_order)-1):
+        if page_order[i] in rules[page_order[i+1]]:
+            return False
+    return True
+
+# Parse data
+file = open('./data/day5.txt')
+data = file.read().split('\n\n')
+rules = {}
+for rule in data[0].split('\n'):
+    rule = list(map(int, rule.split('|')))
+    rules[rule[0]] = rules.get(rule[0], set()) | set([rule[1]])
+
+page_orders = [list(map(int, order.split(','))) for order in data[1].split('\n')]
+
+start = time.perf_counter()
+result = 0
+for page_order in page_orders:
+    if is_in_order(page_order, rules): result+=page_order[len(page_order)//2]
+end = time.perf_counter()
+
+duration_ns = (end-start)
+print(f"It took {duration_ns}ns to calculate {result}")
